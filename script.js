0043fe46292bd1e52f65f06103396367870fe1fb
henriB <henri.bretseau@gmail.com>
2020-10-25 12:37:20 +0100

    Navbar script scroll

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// NAVBAR
+let lastscrollTop= 0;
+navbar = window.document.getElementById('navbar');
+
+window.addEventListener('scroll',function(){
+    const scrollTop = window.pageYOffset || this.document.documentElement.scrollTop;
+    if (scrollTop > lastscrollTop) {
+        navbar.style.top="-50px";
+    } else {
+        navbar.style.top="0";
+    }
+    lastscrollTop = scrollTop;
+})
 // TYPED
 var typed = new Typed('.typed', {
     strings: [ "Bonjour à tous !","Je me présente, je m appelle Henri. Je voudrais bien réussir ma vie, être aimé , être beau, gagner de l argent. Puis surtout être intelligent Mais pour tout ça il faudrait que je bosse à plein temps . Je suis chanteur, je chante pour mes copains .Je veux faire des tubes et que ça tourne bien, tourne bien. Je veux écrire une chanson dans le vent. Un air gai, chic et entraînant. Pour faire danser dans les soirées de Monsieur Durand"],
