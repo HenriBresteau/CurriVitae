7d945ee74b2f71b566438541184cac3c48eca209
henriB <henri.bretseau@gmail.com>
2020-10-25 12:15:49 +0100

    script texte typed

---
+++ script.js
@@ -0,0 +1,6 @@
+// TYPED
+var typed = new Typed('.typed', {
+    strings: [ "Bonjour à tous !","Je me présente, je m appelle Henri. Je voudrais bien réussir ma vie, être aimé , être beau, gagner de l argent. Puis surtout être intelligent Mais pour tout ça il faudrait que je bosse à plein temps . Je suis chanteur, je chante pour mes copains .Je veux faire des tubes et que ça tourne bien, tourne bien. Je veux écrire une chanson dans le vent. Un air gai, chic et entraînant. Pour faire danser dans les soirées de Monsieur Durand"],
+    typeSpeed: 40,
+  });
+
